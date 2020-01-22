be4f9f5b08c774d81f82dbaf78470a8f4ca288af
sosigen <56933255+sosigen@users.noreply.github.com>
2020-01-22 21:22:31 +0100

    bug znaleziony
    (kod psuła pętla while)

--- animacje.js
+++ animacje.js
@@ -1,25 +1,14 @@
 
-/*function stworzIkone(numer){
-    let ikonka = document.createElement('i')
-    ikonka.classList.add('ikonka','icon-user-2' )
-    //dodaje 0, gdy liczba jest jednocyfrowa, by calosc byla bardziej symetryczna
-    numer <= 9 ? ikonka.innerHTML = `<span>0${numer}</span>` : ikonka.innerHTML = `<span>${numer}</span>`
-    document.getElementById('ikony').appendChild(ikonka)
-}*/
 function generujIkony(grupy){
     if(pojemnik.childNodes.length != 0){
-        while (pojemnik.firstChild) {
-            pojemnik.removeChild(pojemnik.firstChild);
-          }
+        $('#ikony').html('');
     }
+    
     for(let i=0; i<grupy.length; i++){
         for(let j=0; j<grupy[i].length; j++){
             stworzIkone(grupy[i][j])
         }
         przerwa()
-        /*let enter = document.createElement('div')
-        enter.style.clear = 'both';
-        pojemnik.appendChild(enter)*/
     }
 }
 
